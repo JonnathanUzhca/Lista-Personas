90fb54e765f51abca241161cac7d726c0db677e7
JonathanUzhca <jonathan.uzhca0106@gmail.com>
2022-01-31 16:52:27 -0500

    Servicio en Angular

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,10 +1,13 @@
+import { ProviderAst } from '@angular/compiler';
 import { Component, EventEmitter, Output } from '@angular/core';
+import { LoggingService } from '../LoggingService.service';
 import { Persona } from '../persona.model';
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
-  styleUrls: ['./formulario.component.css']
+  styleUrls: ['./formulario.component.css'],
+  providers :[LoggingService]
 })
 export class FormularioComponent  {
 
@@ -12,10 +15,13 @@ export class FormularioComponent  {
 
     //nombreInput: string ='';
     //apellidoInput: string ='';
+   constructor(private logginService: LoggingService){
 
+   }
     agregarPersona(NombreInput:HTMLInputElement, ApellidoInput:HTMLInputElement){
       let persona1 = new Persona(NombreInput.value, ApellidoInput.value);
       //this.personas.push(persona1);
+      this.logginService.enviarMensajeAConsola("Enviamos Persona : "+persona1.apellido + persona1.nombre);
       this.personaCreada.emit(persona1);
     }
 
